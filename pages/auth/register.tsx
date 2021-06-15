c1a0e2a723a820d1e6b5787502b81f8929b9f1c9
Philippe <Philimart21@gmail.com>
2021-06-15 16:35:15 +0100

    init

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -10,23 +10,69 @@ const Register = () => {
     resolver: yupResolver(registerSchema),
   });
 
+  const onRegister = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form className="w-96">
+      <form onSubmit={handleSubmit(onRegister)}>
         <h1 className="mb-10 mt-16">Register</h1>
         <h2>Create your account in a few clicks</h2>
         <p className="mt-2 mb-5 text-gray-400 ">Welcome amongs us</p>
 
+        <div className="flex flex-col md:flex-row">
+          <div className="w-full flex flex-col md:pr-2">
+            <label className="mb-2">Firstname *</label>
+            <input
+              className="input-form mb-1"
+              name="firstname"
+              type="text"
+              ref={register}
+            />
+            <small className="mt-1 mb-1 text-red-500">
+              {errors.firstname?.message}
+            </small>
+          </div>
+
+          <div className="flex flex-col w-full">
+            <label className="mb-2">Lastname </label>
+            <input
+              className="input-form mb-1"
+              name="lastname"
+              type="text"
+              ref={register}
+            />
+            <small className="mt-1 mb-1 text-red-500">
+              {errors.lastname?.message}
+            </small>
+          </div>
+        </div>
+
+        <div className="flex flex-col mb-3">
+          <label>Phone</label>
+          <input className="input-form my-2" type="number" />
+          <small className="mt-1 text-red-500">{errors.phone?.message}</small>
+        </div>
+
         <div className="flex flex-col mb-3">
           <label>Password</label>
           <input
-            className="btn-form my-2"
+            className="input-form my-2"
             type="password"
             placeholder="Password"
           />
+          <small className="mt-1 mb-1 text-red-500">
+            {errors.password?.message}
+          </small>
         </div>
 
-        <div className="btn bg-blue-600 text-white  my-5">CREATE ACCOUNT</div>
+        <button
+          type="submit"
+          className="btn w-full bg-blue-600 text-white my-5"
+        >
+          CREATE ACCOUNT
+        </button>
         <p className="my-5 text-center">
           Already have an account ?
           <Link href="/auth/register">
