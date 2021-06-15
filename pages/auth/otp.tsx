43710e3feb29af6188306eafea796e7fbf8522ad
philippetedajo <philimart21@gmail.com>
2021-06-15 20:54:46 +0100

    update

--- pages/auth/otp.tsx
+++ pages/auth/otp.tsx
@@ -1,6 +1,17 @@
 import AuthTemplate from "../../templates/auth.template";
+import OtpInput from "react-otp-input";
+import { useState } from "react";
 
 const Otp = () => {
+  const [code, setCode] = useState("");
+  const [error, setError] = useState(false);
+
+  const onChange = (otp) => {
+    setCode(otp);
+  };
+
+  console.log(code);
+
   return (
     <div className="w-96 lg:w-2/5">
       <div>
@@ -9,11 +20,22 @@ const Otp = () => {
 
         <p className="mt-2 mb-5 text-gray-400 ">Enter the code you receive</p>
 
+        <OtpInput
+          value={code}
+          onChange={onChange}
+          numInputs={6}
+          inputStyle="inputStyle"
+          errorStyle="error"
+          shouldAutoFocus
+          separator={<span>-</span>}
+          className="py-8 w-ful"
+        />
+
         <button
           type="submit"
           className="btn w-full bg-blue-600 text-white mt-3 pb-3"
         >
-          SIGN IN
+          Submit
         </button>
         <p className="my-5">
           No Otp code ?{" "}
