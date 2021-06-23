21fbee7bdb4da050bc012c9d049a0ba812515a98
phil <philippe.tedajo@gmail.com>
2021-06-23 23:49:15 +0100

    update

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,6 +1,25 @@
 import withSession from "../../../utils/session";
+import axios from "axios";
 
 export default withSession(async (req, res) => {
-  req.session.destroy();
-  res.json({ isLoggedIn: false, data: null });
+  const url = `${process.env.API_URL}/auth/logout`;
+  const user = req.session.get("user");
+
+  try {
+    const data = await axios.get(url, {
+      headers: {
+        Authorization: user.data.token,
+      },
+    });
+
+    if (data.data.code != 200) {
+      res.json({ data: data.data });
+      return;
+    }
+
+    req.session.destroy();
+    res.json({ isLoggedIn: false, data: data.data });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
