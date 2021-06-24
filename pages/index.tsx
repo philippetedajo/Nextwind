aa93b80aceceda3b93eb098a890539168e9e6f11
phil <philippe.tedajo@gmail.com>
2021-06-24 13:06:51 +0100

    up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import Head from "next/head";
 import PageTemplate from "../templates/page.template";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../context";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const { checkSession, login, user, isLoading } = useContext(AuthContext);
+  const router = useRouter();
+
+  useEffect(() => {
+    checkSession();
+    if (user && user?.isLoggedIn) {
+      router.push("/dashboard");
+    }
+  }, [user?.isLoggedIn]);
+
   return (
     <div>
       <Head>
